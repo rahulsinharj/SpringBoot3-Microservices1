ee8399ead5134d9bcdb4337e1162b7331291f45b
Rahul Sinha <rahulkumarsinha1234@gmail.com>
2026-06-20 23:52:14 +0530

    department service

---
+++ department-service/src/test/java/rahul_microservices/department_service/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rahul_microservices.department_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
